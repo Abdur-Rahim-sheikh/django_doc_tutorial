44d75999364073e4475ce954e97a67b7f99b8352
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2023-11-20 21:01:54 +0600

    poll index test case created

--- polls/models.py
+++ polls/models.py
@@ -7,13 +7,14 @@ from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return f"{self.question_text[:20]}... {self.pub_date}"
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
